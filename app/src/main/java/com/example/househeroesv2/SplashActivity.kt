7912bb283a6814e87df2c3452898362070a7dd45
Isabella Valentino <isabella_valentino@ymail.com>
2024-12-05 18:22:14 -0500

    Fixes to splash activity and gradiant added.

--- app/src/main/java/com/example/househeroesv2/SplashActivity.kt
+++ app/src/main/java/com/example/househeroesv2/SplashActivity.kt
@@ -1,20 +1,21 @@
 package com.example.househeroesv2
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Delay for 3 seconds, then navigate to MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000ms = 3 seconds
     }
 }
