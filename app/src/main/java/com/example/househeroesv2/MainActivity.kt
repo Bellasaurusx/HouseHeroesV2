5e223c842eb45fe62b2a1f996c4e2345227f5116
Marcos Luna-Nieblas <MALunaNieblas@student.fullsail.edu>
2024-12-16 10:42:21 -0800

    fixed color

--- app/src/main/java/com/example/househeroesv2/MainActivity.kt
+++ app/src/main/java/com/example/househeroesv2/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.replace
 import com.example.househeroesv2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
