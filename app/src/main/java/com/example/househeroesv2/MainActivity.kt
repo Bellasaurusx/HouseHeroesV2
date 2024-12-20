4fbdcb0986ded1beb03d90b4182b1a8b85a49ad3
Marcos Luna-Nieblas <MALunaNieblas@student.fullsail.edu>
2024-12-19 16:12:54 -0800

    new branch

--- app/src/main/java/com/example/househeroesv2/MainActivity.kt
+++ app/src/main/java/com/example/househeroesv2/MainActivity.kt
@@ -10,7 +10,7 @@ import com.example.househeroesv2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     val sharedViewModel: SharedViewModel by viewModels() // added for the coins
-
+// new bramch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
